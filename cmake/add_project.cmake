7926ea938f11fe4ef561f667da99966e71f4354e
xiaoming <xiaoming@airlango.com>
2019-02-02 20:08:46 +0800

    wip

--- cmake/add_project.cmake
+++ cmake/add_project.cmake
@@ -31,51 +31,46 @@
 #
 ############################################################################
 
-include(px4_base)
+include(base)
 
 #=============================================================================
 #
-#	px4_add_module
+#	add_project
 #
-#	This function builds a static library from a module description.
+#	This function builds a static library from a project description.
 #
 #	Usage:
-#		px4_add_module(MODULE <string>
+#		add_project(PROJECT <string>
 #			MAIN <string>
-#			[ STACK <string> ] !!!!!DEPRECATED, USE STACK_MAIN INSTEAD!!!!!!!!!
 #			[ STACK_MAIN <string> ]
 #			[ STACK_MAX <string> ]
 #			[ COMPILE_FLAGS <list> ]
 #			[ INCLUDES <list> ]
 #			[ DEPENDS <string> ]
 #			[ SRCS <list> ]
-#			[ MODULE_CONFIG <list> ]
+#			[ PROJECT_CONFIG <list> ]
 #			[ EXTERNAL ]
-#			[ DYNAMIC ]
 #			)
 #
 #	Input:
-#		MODULE			: unique name of module
-#		MAIN			: entry point
-#		STACK			: deprecated use stack main instead
-#		STACK_MAIN		: size of stack for main function
+#		PROJECT			: unique name of project
+#		MAIN				: entry point
+#		STACK_MAIN	: size of stack for main function
 #		STACK_MAX		: maximum stack size of any frame
 #		COMPILE_FLAGS		: compile flags
 #		LINK_FLAGS		: link flags
 #		SRCS			: source files
-#		MODULE_CONFIG		: yaml config file(s)
+#		PROJECT_CONFIG		: yaml config file(s)
 #		INCLUDES		: include directories
-#		DEPENDS			: targets which this module depends on
-#		EXTERNAL		: flag to indicate that this module is out-of-tree
-#		DYNAMIC			: don't compile into the px4 binary, but build a separate dynamically loadable module (posix)
-#		UNITY_BUILD		: merge all source files and build this module as a single compilation unit
+#		DEPENDS			: targets which this project depends on
+#		EXTERNAL		: flag to indicate that this project is out-of-tree
+#		UNITY_BUILD		: merge all source files and build this project as a single compilation unit
 #
 #	Output:
-#		Static library with name matching MODULE.
-#		(Or a shared library when DYNAMIC is specified.)
+#		Static library with name matching PROJECT.
 #
 #	Example:
-#		px4_add_module(MODULE test
+#		add_project(PROJECT test
 #			SRCS
 #				file.cpp
 #			STACK_MAIN 1024
@@ -83,40 +78,40 @@ include(px4_base)
 #				git_nuttx
 #			)
 #
-function(px4_add_module)
+function(add_project)
 
 	px4_parse_function_args(
-		NAME px4_add_module
-		ONE_VALUE MODULE MAIN STACK STACK_MAIN STACK_MAX PRIORITY
+		NAME add_project
+		ONE_VALUE PROJECT MAIN STACK STACK_MAIN STACK_MAX PRIORITY
 		MULTI_VALUE COMPILE_FLAGS LINK_FLAGS SRCS INCLUDES DEPENDS MODULE_CONFIG
 		OPTIONS EXTERNAL DYNAMIC UNITY_BUILD
-		REQUIRED MODULE MAIN
+		REQUIRED PROJECT MAIN
 		ARGN ${ARGN})
 
 	if(UNITY_BUILD AND (${PX4_PLATFORM} STREQUAL "nuttx"))
 		# build standalone test library to catch compilation errors and provide sane output
-		add_library(${MODULE}_original STATIC EXCLUDE_FROM_ALL ${SRCS})
+		add_library(${PROJECT}_original STATIC EXCLUDE_FROM_ALL ${SRCS})
 		if(DEPENDS)
-			add_dependencies(${MODULE}_original ${DEPENDS})
+			add_dependencies(${PROJECT}_original ${DEPENDS})
 		endif()
 
 		if(INCLUDES)
-			target_include_directories(${MODULE}_original PRIVATE ${INCLUDES})
+			target_include_directories(${PROJECT}_original PRIVATE ${INCLUDES})
 		endif()
-		target_compile_definitions(${MODULE}_original PRIVATE PX4_MAIN=${MAIN}_app_main)
-		target_compile_definitions(${MODULE}_original PRIVATE MODULE_NAME="${MAIN}_original")
+		target_compile_definitions(${PROJECT}_original PRIVATE PX4_MAIN=${MAIN}_app_main)
+		target_compile_definitions(${PROJECT}_original PRIVATE MODULE_NAME="${MAIN}_original")
 
 		# unity build
-		add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${MODULE}_unity.cpp
-			COMMAND cat ${SRCS} > ${CMAKE_CURRENT_BINARY_DIR}/${MODULE}_unity.cpp
-			DEPENDS ${MODULE}_original ${DEPENDS} ${SRCS}
-			COMMENT "${MODULE} merging source"
+		add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT}_unity.cpp
+			COMMAND cat ${SRCS} > ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT}_unity.cpp
+			DEPENDS ${PROJECT}_original ${DEPENDS} ${SRCS}
+			COMMENT "${PROJECT} merging source"
 			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 			)
-		set_source_files_properties(${CMAKE_CURRENT_BINARY_DIR}/${MODULE}_unity.cpp PROPERTIES GENERATED true)
+		set_source_files_properties(${CMAKE_CURRENT_BINARY_DIR}/${PROJECT}_unity.cpp PROPERTIES GENERATED true)
 
-		add_library(${MODULE} STATIC EXCLUDE_FROM_ALL ${CMAKE_CURRENT_BINARY_DIR}/${MODULE}_unity.cpp)
-		target_include_directories(${MODULE} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
+		add_library(${PROJECT} STATIC EXCLUDE_FROM_ALL ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT}_unity.cpp)
+		target_include_directories(${PROJECT} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
 
 		if(DEPENDS)
 			# using target_link_libraries for dependencies provides linking
@@ -124,40 +119,40 @@ function(px4_add_module)
 			foreach(dep ${DEPENDS})
 				get_target_property(dep_type ${dep} TYPE)
 				if (${dep_type} STREQUAL "STATIC_LIBRARY")
-					target_link_libraries(${MODULE}_original PRIVATE ${dep})
+					target_link_libraries(${PROJECT}_original PRIVATE ${dep})
 				else()
-					add_dependencies(${MODULE}_original ${dep})
+					add_dependencies(${PROJECT}_original ${dep})
 				endif()
 			endforeach()
 		endif()
 
 	elseif(DYNAMIC AND MAIN AND (${OS} STREQUAL "posix"))
-		add_library(${MODULE} SHARED ${SRCS})
-		target_compile_definitions(${MODULE} PRIVATE ${MAIN}_main=px4_module_main)
-		set_target_properties(${MODULE} PROPERTIES
+		add_library(${PROJECT} SHARED ${SRCS})
+		target_compile_definitions(${PROJECT} PRIVATE ${MAIN}_main=px4_project_main)
+		set_target_properties(${PROJECT} PROPERTIES
 			PREFIX ""
 			SUFFIX ".px4mod"
 			)
-		target_link_libraries(${MODULE} PRIVATE px4)
+		target_link_libraries(${PROJECT} PRIVATE px4)
 		if(APPLE)
 			# Postpone resolving symbols until loading time, which is the default on most systems, but not Mac.
-			set_target_properties(${MODULE} PROPERTIES LINK_FLAGS "-undefined dynamic_lookup")
+			set_target_properties(${PROJECT} PROPERTIES LINK_FLAGS "-undefined dynamic_lookup")
 		endif()
 
 	else()
-		add_library(${MODULE} STATIC EXCLUDE_FROM_ALL ${SRCS})
+		add_library(${PROJECT} STATIC EXCLUDE_FROM_ALL ${SRCS})
 	endif()
 
-	# all modules can potentially use parameters and uORB
-	add_dependencies(${MODULE} uorb_headers)
+	# all projects can potentially use parameters and uORB
+	add_dependencies(${PROJECT} uorb_headers)
 
 	if(NOT DYNAMIC)
-		target_link_libraries(${MODULE} PRIVATE prebuild_targets parameters_interface platforms__common px4_layer systemlib)
-		set_property(GLOBAL APPEND PROPERTY PX4_MODULE_LIBRARIES ${MODULE})
+		target_link_libraries(${PROJECT} PRIVATE prebuild_targets parameters_interface platforms__common px4_layer systemlib)
+		set_property(GLOBAL APPEND PROPERTY PX4_MODULE_LIBRARIES ${PROJECT})
 		set_property(GLOBAL APPEND PROPERTY PX4_MODULE_PATHS ${CMAKE_CURRENT_SOURCE_DIR})
 	endif()
 
-	# Pass variable to the parent px4_add_module.
+	# Pass variable to the parent add_project.
 	set(_no_optimization_for_target ${_no_optimization_for_target} PARENT_SCOPE)
 
 	# set defaults if not set
@@ -169,34 +164,34 @@ function(px4_add_module)
 		if(NOT ${property})
 			set(${property} ${${property}_DEFAULT})
 		endif()
-		set_target_properties(${MODULE} PROPERTIES ${property} ${${property}})
+		set_target_properties(${PROJECT} PROPERTIES ${property} ${${property}})
 	endforeach()
 
 	# default stack max to stack main
 	if(NOT STACK_MAX)
 		set(STACK_MAX ${STACK_MAIN})
 	endif()
-	set_target_properties(${MODULE} PROPERTIES STACK_MAX ${STACK_MAX})
+	set_target_properties(${PROJECT} PROPERTIES STACK_MAX ${STACK_MAX})
 
 	if(${PX4_PLATFORM} STREQUAL "qurt")
-		set_property(TARGET ${MODULE} PROPERTY POSITION_INDEPENDENT_CODE TRUE)
+		set_property(TARGET ${PROJECT} PROPERTY POSITION_INDEPENDENT_CODE TRUE)
 	elseif(${PX4_PLATFORM} STREQUAL "nuttx")
-		target_compile_options(${MODULE} PRIVATE -Wframe-larger-than=${STACK_MAX})
+		target_compile_options(${PROJECT} PRIVATE -Wframe-larger-than=${STACK_MAX})
 	endif()
 
 	if(MAIN)
-		target_compile_definitions(${MODULE} PRIVATE PX4_MAIN=${MAIN}_app_main)
-		target_compile_definitions(${MODULE} PRIVATE MODULE_NAME="${MAIN}")
+		target_compile_definitions(${PROJECT} PRIVATE PX4_MAIN=${MAIN}_app_main)
+		target_compile_definitions(${PROJECT} PRIVATE MODULE_NAME="${MAIN}")
 	else()
-		target_compile_definitions(${MODULE} PRIVATE MODULE_NAME="${MODULE}")
+		target_compile_definitions(${PROJECT} PRIVATE MODULE_NAME="${PROJECT}")
 	endif()
 
 	if(COMPILE_FLAGS)
-		target_compile_options(${MODULE} PRIVATE ${COMPILE_FLAGS})
+		target_compile_options(${PROJECT} PRIVATE ${COMPILE_FLAGS})
 	endif()
 
 	if(INCLUDES)
-		target_include_directories(${MODULE} PRIVATE ${INCLUDES})
+		target_include_directories(${PROJECT} PRIVATE ${INCLUDES})
 	endif()
 
 	if(DEPENDS)
@@ -205,22 +200,22 @@ function(px4_add_module)
 		foreach(dep ${DEPENDS})
 			get_target_property(dep_type ${dep} TYPE)
 			if (${dep_type} STREQUAL "STATIC_LIBRARY")
-				target_link_libraries(${MODULE} PRIVATE ${dep})
+				target_link_libraries(${PROJECT} PRIVATE ${dep})
 			else()
-				add_dependencies(${MODULE} ${dep})
+				add_dependencies(${PROJECT} ${dep})
 			endif()
 		endforeach()
 	endif()
 
 	foreach (prop LINK_FLAGS STACK_MAIN MAIN PRIORITY)
 		if (${prop})
-			set_target_properties(${MODULE} PROPERTIES ${prop} ${${prop}})
+			set_target_properties(${PROJECT} PROPERTIES ${prop} ${${prop}})
 		endif()
 	endforeach()
 
 	if(MODULE_CONFIG)
-		foreach(module_config ${MODULE_CONFIG})
-			set_property(GLOBAL APPEND PROPERTY PX4_MODULE_CONFIG_FILES ${CMAKE_CURRENT_SOURCE_DIR}/${module_config})
+		foreach(project_config ${MODULE_CONFIG})
+			set_property(GLOBAL APPEND PROPERTY PX4_MODULE_CONFIG_FILES ${CMAKE_CURRENT_SOURCE_DIR}/${project_config})
 		endforeach()
 	endif()
 endfunction()
